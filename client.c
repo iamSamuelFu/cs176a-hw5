db7ed81441a097303c6e14cfafd87ab044195028
Jie Fu <samuel@JiedeMacBook-Pro.local>
2019-06-07 15:56:17 -0700

    25

--- client.c
+++ client.c
@@ -140,7 +140,7 @@ int main(int argc, char* argv[]) {
                 guessValid = 0;
 
                 while (!guessValid) {
-                    if (guess < 'a' || guess > 'z') {
+                    if (strlen(guess) > 1 || guess < 'a' || guess > 'z') {
                         printf("\nError! Please guess one letter.\nLetter to guess: ");
                         guess = tolower(getchar());
                         getchar();
