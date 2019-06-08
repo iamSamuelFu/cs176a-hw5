f7d792bbfd668fc009b9577556bf0e3816c339bd
Jie Fu <samuel@JiedeMacBook-Pro.local>
2019-06-07 20:54:54 -0700

    all

--- pa/hangman_client.c
+++ pa/hangman_client.c
@@ -99,7 +99,7 @@ int main( int argc, char **argv )
               needInput = 0;
             }
         }
-        if(needInput){
+        while(needInput){
             printf("\n%s","Letter to guess: ");
             char inputs2[128];
             int size2 = 1;
@@ -114,15 +114,16 @@ int main( int argc, char **argv )
             inputs2[0] = (--size2)+'0';
             int len = buffer[1]-'0';
             int incorr = buffer[2] - '0';
+            needInput = 1;
             for(int i = len+3; i<incorr+len+3; i++){
               if(inputs2[1] == buffer[i]){
                 printf("Error! Letter %c has been guessed before, please guess another letter.", inputs2[1]);
+                needInput = 0;
                 break;
               }
             }
-
-            send(fd, inputs2, sizeof(inputs2), 0);
         }
+        send(fd, inputs2, sizeof(inputs2), 0);
         if(isEnd) break;
     }    
 
